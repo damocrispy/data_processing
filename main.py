748d062a22ca94f4337626dcae00a518dc61bae1
damocrispy <damian.christie@pm.me>
2021-06-27 10:52:28 +0100

    Decoding + decompression.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import base64
+import zlib
 
 with open('function_input.json', 'r') as input_json:
     input_dict = json.load(input_json)
@@ -13,7 +15,10 @@ for record in input_dict['records']:
         #   ...initialise it's payload list.
         vehicles[record['vehicle_id']] = []
 
-    #   Add the payload to the payload list.
-    vehicles[record['vehicle_id']].append(record['payload'])
+    #   Decode payload from base 64 then decompress.
+    payload = record['payload']
+    pl_decoded = base64.b64decode(payload)
+    pl_decomp = zlib.decompress(pl_decoded)
 
-print(vehicles)
+    #   Add the payload to the payload list.
+    vehicles[record['vehicle_id']].append(pl_decomp)
